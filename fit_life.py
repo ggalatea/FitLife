1f47919431277815d8b79164f5b7cd15462b5f17
Anna Semenyuchenko <ggalatea@yandex.ru>
2026-06-02 12:14:54 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -1,4 +1,8 @@
 # Проект FitLife - MVP версия 2.0
+WATER_PER_KG = 30
+WATER_CONVERT_TO_LITERS = 1000
+ROUNDING_UP = 1
+
 import sys
 
 sys.stdin.reconfigure(encoding="utf-8")
@@ -14,17 +18,25 @@ while True:
         print("Пожалуйста, введи возвраст, используя цифры")
 
 # 2. Сбор данных
-user_weight = float(input("Какой у тебя вес?(например, 52)").replace(",", "."))
-user_height = float(input("Какого ты рост?(например, 1.75)").replace(",", "."))
+while True:
+    try:
+        user_weight = float(input("Какой у тебя вес?(например, 52)").replace(",", "."))
+        break  # Выход из цикла при успешном преобразовании
+    except ValueError:
+        print("Пожалуйста, введи вес, используя цифры")
+
+while True:
+    try:
+        user_height = float(input("Какого ты рост?(например, 1.75)").replace(",", "."))
+        break  # Выход из цикла при успешном преобразовании
+    except ValueError:
+        print("Пожалуйста, введи рост, используя цифры")
+
 
 # 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-bmi = round((user_weight / (user_height**2)), 1)  # расчёт индекса массы тела
-
-# Подсчет воды: вес * 30 мл
-WATER_PER_KG = 30
-WATER_CONVERT_TO_LITERS = 1000
+bmi = round((user_weight / (user_height**2)), ROUNDING_UP)  # расчёт индекса массы тела
 water_ml = user_weight * WATER_PER_KG
-water_l = round((water_ml / WATER_CONVERT_TO_LITERS), 1)  # норма воды в литрах
+water_l = round((water_ml / WATER_CONVERT_TO_LITERS), ROUNDING_UP)
 
 # 4. Вывод красивого результата
 print(f"Расчёт для {user_name}, {user_age} лет")
